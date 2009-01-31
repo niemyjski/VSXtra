8c73cb77c4c418fe8429812d0605e19008cabd1a
Istvan Novak <Dotneteer@users.noreply.github.com>
2009-01-31 12:13:15 +0000

    Bug preventing package load in PackageBase.cs has been fixed.

--- VSXTra/UnitTests/VSXtra.Test.Functional/Package/RegistrationAttributes/XtraProvideAutoLoadAttributeTest.cs
+++ VSXTra/UnitTests/VSXtra.Test.Functional/Package/RegistrationAttributes/XtraProvideAutoLoadAttributeTest.cs
@@ -1,27 +1,27 @@
 using System;
-using NUnit.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VSXtra.Package;
 
 namespace VSXtra.Test.Functional.Package.RegistrationAttributes
 {
-  [TestFixture]
+  [TestClass]
   public class XtraProvideAutoLoadAttributeTest
   {
-    [Test]
+    [TestMethod]
     public void UIContextGuidIsAccepted()
     {
       var attr = new XtraProvideAutoLoadAttribute(typeof (UIContext.NoSolution));
       Assert.AreEqual(attr.LoadGuid, typeof(UIContext.NoSolution).GUID);
     }
 
-    [Test]
+    [TestMethod]
     [ExpectedException(typeof(ArgumentException))]
     public void WrongUIContextTypeIsRefused1()
     {
       new XtraProvideAutoLoadAttribute(typeof(WindowKind.CallStack));
     }
 
-    [Test]
+    [TestMethod]
     [ExpectedException(typeof(ArgumentException))]
     public void WrongUIContextTypeIsRefused2()
     {
